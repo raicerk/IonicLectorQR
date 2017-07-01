accffb1163308ccf0fba2f2abb2035498cff35d1
Juan Mora <raicerk@gmail.com>
2017-07-01 12:16:51 -0400

    Prueba funcional de la aplicación, aplicación impimiendo los datos por consola en modo dedepuración

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	dato : any;
 
-  }
+	constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner) {
+    console.log("Iniciado el sistema");
+	}
 
+	ScanearCodigo(){
+		this.barcodeScanner.scan().then((barcodeData) => {
+			// Success! Barcode data is here
+      console.log(barcodeData);
+			this.dato = barcodeData;
+		}, (err) => {
+			// An error occurred
+      console.log(err);
+		});
+	}
 }
